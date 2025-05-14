bbf8a8c781b876ee7db07e1e30c23d82e3221e58
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2025-05-14 23:06:06 +0100

    update

--- lib/model.ml
+++ lib/model.ml
@@ -1,6 +1,5 @@
 open Sexplib.Std
 
-
 type element =
   (* syntactic components that at the same or greater than a paragraph *)
   | Heading of heading_info
--- lib/parse.ml
+++ lib/parse.ml
@@ -1,6 +1,6 @@
 module A = Angstrom
 
-module P = struct
+module Primitive = struct
   let is_blank = function
     | ' ' | '\t' | '\n' | '\r' -> true
     | _ -> false
@@ -10,13 +10,20 @@ module P = struct
     | '0' .. '9' -> true
     | _ -> false
   ;;
+
+  let is_star = function
+    | '*' -> true
+    | _ -> false
+  ;;
 end
+
+module P = Primitive
 
 let unit = A.return ()
 
 let digit =
   let open A in
-  satisfy P.is_digit
+  A.satisfy P.is_digit
   >>| function
   | '0' -> 0
   | '1' -> 1
@@ -30,3 +37,16 @@ let digit =
   | '9' -> 9
   | _ -> assert false
 ;;
+
+let stars =
+  let open A in
+  A.take_while P.is_star
+  >>= fun raw_stars ->
+  if String.length raw_stars = 0
+  then fail "no stars"
+  else if String.length raw_stars > 6
+  then fail "too many stars"
+  else return (String.length raw_stars)
+;;
+
+let colon = A.char ':'
