0161128b809b8177fa251c0bd0cd71f1f579212f
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2025-07-08 11:57:13 +0100

    move primitive into separate module

---
+++ lib/primitive.ml
@@ -0,0 +1,94 @@
+let is_space = function
+  | ' ' -> true
+  | _ -> false
+;;
+
+let is_blank = function
+  | ' ' | '\t' -> true
+  | _ -> false
+;;
+
+let is_newline = function
+  | '\n' | '\r' -> true
+  | _ -> false
+;;
+
+let is_whitespace c = is_blank c || is_newline c
+
+let is_numeric = function
+  | '0' .. '9' -> true
+  | _ -> false
+;;
+
+let is_alpha = function
+  | 'a' .. 'z' | 'A' .. 'Z' -> true
+  | _ -> false
+;;
+
+let is_alpha_numeric = function
+  | 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' -> true
+  | _ -> false
+;;
+
+let is_star = function
+  | '*' -> true
+  | _ -> false
+;;
+
+let is_special_char = function
+  | '*'
+  | '/'
+  | '_'
+  | '+'
+  | '~'
+  | '='
+  | '['
+  | '<'
+  | '{'
+  | '\\'
+  | '@'
+  | '$'
+  | '\n'
+  | '\r' -> true
+  | _ -> false
+;;
+
+let is_path_char = function
+  | ' ' | '\t' | '\n' | '\r' | '(' | ')' | '[' | ']' | '<' | '>' -> false
+  | _ -> true
+;;
+
+let is_punct_char = function
+  (* https://support.google.com/a/answer/1371415?hl=en *)
+  | '!'
+  | '"'
+  | '#'
+  | '$'
+  | '%'
+  | '&'
+  | '\''
+  | '('
+  | ')'
+  | '*'
+  | '+'
+  | ','
+  | '\\'
+  | '-'
+  | '/'
+  | ':'
+  | ';'
+  | '<'
+  | '='
+  | '>'
+  | '?'
+  | '@'
+  | '['
+  | ']'
+  | '^'
+  | '_'
+  | '`'
+  | '{'
+  | '|'
+  | '}' -> true
+  | _ -> false
+;;
